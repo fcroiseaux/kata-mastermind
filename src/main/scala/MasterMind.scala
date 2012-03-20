d22c0d5a2f6efcf5fd99f5ff7c9d6914d2e7a028
Fabrice Croiseaux <fcroiseaux@gmail.com>
2012-03-20 10:53:46 +0100

    Refactoring de la génération des coups en plus fonctionnel

--- src/main/scala/MasterMind.scala
+++ src/main/scala/MasterMind.scala
@@ -8,54 +8,58 @@
 package lu.ade.kata
 
 // Le mastermind, avec le code secret et la liste des couleurs
-class MasterMind(private val secret:List[String], val values:List[String]) {
+class MasterMind(private val secret: List[String], val values: List[String]) {
 
-  def secretLen=secret.size      // La longueur du code secret (nb de pions)
-  def valuesLen=values.size      // Le nombre de couleurs possibles
+  def secretLen = secret.size // La longueur du code secret (nb de pions)
+  def valuesLen = values.size // Le nombre de couleurs possibles
 
   // Vérifie une proposition, en retournant le nombre bien placé et le nombre mal placé
-  def check(proposition:List[String])={
+  def check(proposition: List[String]) = {
     assert(proposition.size == secret.size)
-    val (placed,incorrect)=proposition.zip(secret).partition(v=>v._1.equals(v._2))
-    val rest=incorrect.map(_._2)
-    (placed.size,incorrect.count(v=>rest.contains(v._1)))
+    val (placed, incorrect) = proposition.zip(secret).partition(v => v._1.equals(v._2))
+    val rest = incorrect.map(_._2)
+    (placed.size, incorrect.count(v => rest.contains(v._1)))
   }
 
   // Retourne le nombre total de possibilités pour ce mastermind
-  def max=math.pow(valuesLen,secretLen).toInt
+  def max = math.pow(valuesLen, secretLen).toInt
 
 }
 
 // Génère des combinaisons candidates pour la resolution du mastermind
-class Generator(val mastermind:MasterMind){
+class Generator(val mastermind: MasterMind) {
+
+  // Conversion d'un entier en une liste le représentant en base n
+  def baseN(index: Int, base: Int): List[Int] = index match {
+    case zero: Int if zero == 0 => List.fill(mastermind.secretLen) {0}
+    case _ => index % base :: baseN(index / base, base)
+  }
 
   // Retourne une possibilité à un index donné (de 0 à mastermind.max-1)
-  def propositionAt(index:Int):List[String]={
-    for(i <- (0 until mastermind.secretLen).toList)
-      yield mastermind.values(
-        index%
-          math.pow(mastermind.valuesLen,mastermind.secretLen-i).toInt/
-            (math.pow(mastermind.valuesLen,mastermind.secretLen-i).toInt/mastermind.valuesLen)
-      )
-  }
+  def propositionAt(index: Int): List[String] =
+    baseN(index, mastermind.valuesLen).take(mastermind.secretLen).reverse.map { mastermind.values }
+
 
   // Stream une à une chacune des propositions
-  private def streamPropositions(index:Int):Stream[List[String]]=index match {
-    case s:Int if s>=mastermind.max => Stream.empty
-    case _ => Stream.cons(propositionAt(index),streamPropositions(index+1))
+  private def streamPropositions(index: Int): Stream[List[String]] = index match {
+    case s: Int if s >= mastermind.max => Stream.empty
+    case _ => Stream.cons(propositionAt(index), streamPropositions(index + 1))
   }
 
-  def streamAll=streamPropositions(0)
+  def streamAll = streamPropositions(0)
 }
 
-trait Solver{
-  def mastermind:MasterMind
-  def solve:Option[List[String]]
+trait Solver {
+  def mastermind: MasterMind
+
+  def solve: Option[List[String]]
 }
 
-class BruteSolver(val mastermind:MasterMind) extends Solver {
+class BruteSolver(val mastermind: MasterMind) extends Solver {
 
   // Fin à la première qui match le code secret
-  def solve=new Generator(mastermind).streamAll.find({c=>mastermind.check(c)==(mastermind.secretLen,0)})
+  def solve = new Generator(mastermind).streamAll.find({
+    c => mastermind.check(c) ==(mastermind.secretLen, 0)
+  })
 
 }
